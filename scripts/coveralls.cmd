4df9d84d231ddf58c55547f0b13d2e58e5188c11
Anderson Rancan <anderson.rancan@gmail.com>
2017-01-08 12:49:52 +0100

    added scripts for code coverage, coveralls, opencover and report generator for opencover

---
+++ scripts/coveralls.cmd
@@ -0,0 +1,8 @@
+pushd %1
+
+for /f %%i in ('git rev-parse HEAD') do set commit_id=%%i
+for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set commit_branch=%%i
+
+.\packages\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i ./coverage.xml --repoToken srgzmvxl5EkKPV5eD1gmU92LWNO9rOxza --commitId %commit_id% --commitBranch %commit_branch% --commitAuthor "anderson-rancan" --commitEmail "anderson.rancan@gmail.com" --commitMessage "local build"
+
+popd
